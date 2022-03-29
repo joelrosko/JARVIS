d6f484925f14925e14f64709aecd89a8bdc5806b
Joel Rosko <joel.rosko97@gmail.com>
2022-03-29 13:41:31 +0200

    Login page and basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Box from '@mui/system/Box';
 import { Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import LogInPage from './pages/LogInPage';
@@ -21,12 +22,20 @@ function App() {
 
   return (
     <ThemeProvider theme={darkTheme}>
-      <p>Hello World!</p>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/home' element={<HomePage />} />
-        <Route path='/login' element={<LogInPage />} />
-      </Routes>
+      <Box sx={{display: {xs: 'none', md: 'flex'}, width: '70%', margin: '0 auto'}}>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/home' element={<HomePage />} />
+          <Route path='/login' element={<LogInPage />} />
+        </Routes>
+      </Box>
+      <Box sx={{display: {xs: 'flex', md: 'none'}, width: '80%', margin: '0 auto'}}>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/home' element={<HomePage />} />
+          <Route path='/login' element={<LogInPage />} />
+        </Routes>
+      </Box>
     </ThemeProvider>
   );
 }
--- client/src/pages/LogInPage.jsx
+++ client/src/pages/LogInPage.jsx
@@ -1,7 +1,7 @@
 import Grid from '@mui/material/Grid';
 import Box from '@mui/system/Box';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import InputField from '../components/base_components/InputField';
+import Buttons from '../components/base_components/Buttons';
 import Typography from '@mui/material/Typography';
 import HeadphonesIcon from '@mui/icons-material/Headphones';
 import Link from '@mui/material/Link';
@@ -32,64 +32,35 @@ const LogInPage = () => {
         logIn(userName, psw);
     };
   return (
-        <div>
-            <Box sx={{display: {xs: 'none', md: 'flex'}, width: '70%', margin: '0 auto'}}>
-                <Grid container justifyContent='center'>
-                    <Box component='form' onSubmit={onSubmit} sx={{mt:8, height: 450, width: '25%', minWidth: 300, border: 1, borderColor: 'grey.500', borderRadius: 2, bgcolor:'#212121', '&:hover':{borderColor: 'white'}}}>
-                        <Grid item sx={{display:'flex', flexDirection: 'row', justifyContent: 'center', mt: 4}}>
-                            <HeadphonesIcon color="primary" sx={{fontSize: 62}} />
-                        </Grid>
-                        <Grid item sx={{display:'flex', flexDirection:'row', justifyContent:'center', mt: 2}}>
-                            <Typography variant='h5' sx={{fontWeight: 'regular', color: 'white'}}>JARVIS</Typography>
-                        </Grid>
-                        <Grid item sx={{display:'flex', flexDirection:'row', justifyContent:'center', mt: 6}}>
-                            <TextField label="Username" value={userName} onChange={(e) => setUserName(e.target.value)} sx={{width: '90%'}} />
-                        </Grid>
-                        <Grid item sx={{display:'flex', flexDirection:'row', justifyContent:'center', mt: 2}}>
-                            <TextField label="Password" type='password' value={psw} onChange={(e) => setPsw(e.target.value)} sx={{width: '90%'}} />
-                        </Grid>
-                        <Grid item sx={{display:'flex', flexDirection:'row', justifyContent:'center', mt: 6}}>
-                            <Button variant='outlined' type='submit' sx={{width: '90%'}} >
-                                Sign In
-                            </Button>
-                        </Grid>
-                        <Grid item sx={{display:'flex', flexDirection:'row', justifyContent:'center', mt: 1}}>
-                            <Link href="#" underline="hover">
-                                <Typography>Forgot password?</Typography>
-                            </Link>
-                        </Grid>
-                    </Box>
+        <Grid container justifyContent='center'>
+            <Box component='form' onSubmit={onSubmit} sx={{mt:8, p: 2, height: 450, width: '25%', minWidth: 300, border: 1, borderColor: 'grey.500', borderRadius: 2, bgcolor:'#212121', '&:hover':{borderColor: 'white'}}}>
+                <Grid item sx={{display:'flex', flexDirection: 'row', justifyContent: 'center', mt: 4}}>
+                    <HeadphonesIcon color="primary" sx={{fontSize: 62}} />
+                </Grid>
+                <Grid item sx={{display:'flex', flexDirection:'row', justifyContent:'center', mt: 2}}>
+                    <Typography variant='h5' sx={{fontWeight: 'regular', color: 'white'}}>JARVIS</Typography>
+                </Grid>
+                <Grid item sx={{display:'flex', flexDirection:'row', justifyContent:'center', alignItems: 'center', mt: 6}}>
+                    <InputField label='Username' />
+                </Grid>
+                <Grid item sx={{display:'flex', flexDirection:'row', justifyContent:'center', mt: 2}}>
+                    <InputField label='Password' password={true} />
+                </Grid>
+                <Grid item sx={{display:'flex', flexDirection:'row', justifyContent:'flex-end'}}>
+                    <Link href="#" underline="hover">
+                        <Typography>Forgot password?</Typography>
+                    </Link>
+                </Grid>
+                <Grid item sx={{display:'flex', flexDirection:'row', justifyContent:'center', mt: 6}}>
+                    <Buttons type='submit' label='Sign in' />
+                </Grid>
+                <Grid item sx={{display:'flex', flexDirection:'row', justifyContent:'center', mt: 1}}>
+                    <Link href="#" underline="hover">
+                        <Typography>Create an account</Typography>
+                    </Link>
                 </Grid>
             </Box>
-            <Box sx={{display: {xs: 'flex', md: 'none'}, width: '80%', margin: '0 auto'}}>
-                <Grid container justifyContent='center'>
-                    <Box component='form' onSubmit={onSubmit} sx={{mt:8, height: 450, width: '25%', minWidth: 300, border: 1, borderColor: 'grey.500', borderRadius: 2, bgcolor:'#212121', '&:hover':{borderColor: 'white'}}}>
-                        <Grid item sx={{display:'flex', flexDirection: 'row', justifyContent: 'center', mt: 4}}>
-                            <HeadphonesIcon color="primary" sx={{fontSize: 62}} />
-                        </Grid>
-                        <Grid item sx={{display:'flex', flexDirection:'row', justifyContent:'center', mt: 2}}>
-                            <Typography variant='h5' sx={{fontWeight: 'regular', color: 'white'}}>JARVIS</Typography>
-                        </Grid>
-                        <Grid item sx={{display:'flex', flexDirection:'row', justifyContent:'center', mt: 6}}>
-                            <TextField label="Username" value={userName} onChange={(e) => setUserName(e.target.value)} sx={{width: '90%'}} />
-                        </Grid>
-                        <Grid item sx={{display:'flex', flexDirection:'row', justifyContent:'center', mt: 2}}>
-                            <TextField label="Password" type='password' value={psw} onChange={(e) => setPsw(e.target.value)} sx={{width: '90%'}} />
-                        </Grid>
-                        <Grid item sx={{display:'flex', flexDirection:'row', justifyContent:'center', mt: 6}}>
-                            <Button variant='outlined' type='submit' sx={{width: '90%'}} >
-                                Sign In
-                            </Button>
-                        </Grid>
-                        <Grid item sx={{display:'flex', flexDirection:'row', justifyContent:'center', mt: 1}}>
-                            <Link href="#" underline="hover">
-                                <Typography>Forgot password?</Typography>
-                            </Link>
-                        </Grid>
-                    </Box>
-                </Grid>
-            </Box>
-        </div>
+        </Grid>
   )
 }
 
